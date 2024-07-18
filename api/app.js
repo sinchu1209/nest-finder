94e8c2aa35b1b0e1fa21118c0e9b67acbaee5a19
sinchu1209 <148935200+sinchu1209@users.noreply.github.com>
2024-07-18 12:35:51 +0530

    changes made

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 import authRoute from './routes/auth.route.js'
 import postRoute from './routes/post.route.js';
 import testRoute from './routes/test.route.js';
+import userRoute from "./routes/user.route.js";
 
  const app = express();
 
@@ -16,6 +17,7 @@ import testRoute from './routes/test.route.js';
 
 
  app.use("/api/posts" , postRoute);
+ app.use("/api/users", userRoute);
  app.use("/api/auth" , authRoute);
  app.use("/api/test" , testRoute);
 
